c0981892ca51c3a4e6cae7efdb636ba0a4b4b3a7
Verin1005 <daqingchong0809@gmail.com>
2022-08-29 16:39:28 +0800

    mounted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chains, wagmiClient } from "config/wagmi";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import "styles/globals.css";
 import "styles/rainbowkit.css";
 import { WagmiConfig } from "wagmi";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => setMounted(true), []);
+    if (!mounted) return null;
+
     return (
         <WagmiConfig client={wagmiClient}>
             <RainbowKitProvider chains={chains}>
